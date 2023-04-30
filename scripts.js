bb89c6159fc4caedcb54b6695868f2e9c04713a3
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2023-04-30 15:44:51 +0200

    changes to practice

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 document
        .querySelector('#example')
-       .addEventListener('click',() => console.log('hello'))
+       .addEventListener('click',() => console.log('hello')) //logs hello when the button is clicked
 
 
-
-       window.addEventListener('scroll', () => console.log('scrolled'));
+       window.addEventListener('scroll', () => console.log('scrolled')); // logs 'scrolled' when the scrolling action is used 
