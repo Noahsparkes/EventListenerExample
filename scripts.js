0b0bf3cfdf5ca8606d1bf3e2ddb891ef4eac98c8
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2023-04-30 15:40:26 +0200

    event listener practice_01

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document
+       .querySelector('#example')
+       .addEventListener('click',() => console.log('hello'))
+
+
+
+       window.addEventListener('scroll', () => console.log('scrolled'));
