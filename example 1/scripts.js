dd3e6dabfda108595bfc42a6f04d330165535b34
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2023-04-30 16:10:58 +0200

    example 2 boilerplate

--- example 1/scripts.js
+++ example 1/scripts.js
@@ -3,4 +3,5 @@ document
        .addEventListener('click',() => console.log('hello')) //logs hello when the button is clicked
 
 
-       window.addEventListener('scroll', () => console.log('scrolled')); // logs 'scrolled' when the scrolling action is used 
+window.addEventListener('scroll', () => console.log('scrolled')); // logs 'scrolled' when the scrolling action is used 
+ 
