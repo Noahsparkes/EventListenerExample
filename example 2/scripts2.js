a41b38c4abcc71eed2ccbbfa6261a6f734522c6a
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2023-04-30 16:17:35 +0200

    further changes eg.2

---
+++ example 2/scripts2.js
@@ -0,0 +1,20 @@
+const BACKGROUNDS =[
+    'red',
+    'blue',
+    'green',
+    'purple',
+    'brown',
+    'black',
+]
+
+const buttonsList = document.querySelector('button')
+const buttonsArray = Array.from(buttonList)
+
+/**
+ *  Returns a random color value, based on a random index used on the
+ * {@link BACKGROUNDS}
+ */
+ 
+const calcRandomColor = () => {
+    
+}
