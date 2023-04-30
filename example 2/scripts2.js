0e513b79540b07ba2e0cd799994a5188cf337f5e
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2023-04-30 17:02:08 +0200

    updated example 2

--- example 2/scripts2.js
+++ example 2/scripts2.js
@@ -7,14 +7,34 @@ const BACKGROUNDS =[
     'black',
 ]
 
-const buttonsList = document.querySelector('button')
-const buttonsArray = Array.from(buttonList)
+const buttonsList = document.querySelectorAll('button')
+const buttonsArray = Array.from(buttonsList)
 
 /**
  *  Returns a random color value, based on a random index used on the
  * {@link BACKGROUNDS}
+ * 
+ * @returns {string}
  */
  
+
 const calcRandomColor = () => {
+    const maxIndex  =BACKGROUNDS.length - 1
+    const randomIndex = Math.round(maxIndex * Math.random())
+    return BACKGROUNDS[randomIndex]
+}
+ 
+const handler = (event) => {
+
+    const {target} = event
+    target.style.color = 'white'
+    target.style.background = calcRandomColor()
     
 }
+
+for (const button of buttonsArray) {
+    button.addEventListener('click', handler)
+}
+
+
+// result is that each button needs to generate another color everytime it is clicked
